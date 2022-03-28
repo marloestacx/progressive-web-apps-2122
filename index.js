a79a323f35cf9aa1e22bdbb73a90574547743dee
marloestacx <marloes.tacx@live.nl>
2022-03-28 09:33:38 +0200

    Show data home page

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
+require("dotenv").config();
 const express = require("express");
+const fetch = (...args) =>
+  import("node-fetch").then(({ default: fetch }) => fetch(...args));
+
 const app = express();
 const port = 3000;
 
-app.use(express.static("public"));
 app.set("view engine", "ejs");
+app.set("views", "./views");
 
-app.get("/", async (req, res) => {
-  res.render("home");
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  fetch(`https://www.rijksmuseum.nl/api/nl/collection?key=c5r1klvi&ps=10`)
+    .then((response) => response.json())
+    .then((data) => {
+      res.render("home", {
+        pageTitle: "Rijksmuseum",
+        data: data.artObjects,
+      });
+    })
+    .catch((err) => res.send(err));
 });
 
 app.listen(port, () => {
